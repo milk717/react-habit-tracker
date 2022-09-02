9e5d8a2256972071b65427a4c4f7b9efe5849865
milk717 <robolindasoo@gmail.com>
2022-09-02 23:28:55 +0900

    :sparkles: 날짜 표시하기

--- src/component/HabitCheckBoard.js
+++ src/component/HabitCheckBoard.js
@@ -4,9 +4,26 @@ export default function HabitCheckBoard(
     }
 ) {
 
+    const dateArray = new Date(progress[0].date).dateRange(66);
+
     return (
         <>
-
+            <table>
+                <tbody>
+                    <tr>
+                        <th>
+                            1
+                        </th>
+                        {
+                            dateArray.map((item, index)=>(
+                                <td>
+                                    {item.date.slice(5,10)}
+                                </td>
+                            ))
+                        }
+                    </tr>
+                </tbody>
+            </table>
         </>
     );
 };
